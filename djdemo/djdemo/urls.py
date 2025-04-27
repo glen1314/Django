809251ef67cd0f4646a8cced8249b3fbce5de1d6
glen <1621041508@qq.com>
2025-04-27 22:14:02 +0800

    2025-04-27更新

--- djdemo/djdemo/urls.py
+++ djdemo/djdemo/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+
 from goods import  views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', RedirectView.as_view(url='/admin/')),
     path('index/',views.index,name='index')
 
 ]
